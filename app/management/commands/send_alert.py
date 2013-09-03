e606d44c7fcaf28adbae8c68e23fa33366a57dd7
shpuntik <shpuntik74@gmail.com>
2013-09-03 23:12:55 +0600

    Change script for email alerts

--- app/management/commands/send_alert.py
+++ app/management/commands/send_alert.py
@@ -5,23 +5,35 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core.mail import send_mass_mail
 from django.contrib.auth.models import User
 from app.models import Alert
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 class Command(BaseCommand):
 	args = ''
-	help = 'Run alerts for users'
+	help = 'Send alerts for users'
 
 	def handle(self, *args, **options):
 		cur_time = datetime.now().strftime('%H:%M')
-		alerts = Alert.objects.filter(alert_server_time=cur_time)
-		#alerts = Alert.objects.all()
+		cur_date = date.today()
+		pre_alerts = Alert.objects.filter(alert_server_time=cur_time, start__lte=cur_date, finish__gte=cur_date)
+		alerts = []
 
-		if alerts.count() != 0:
+		for alert in pre_alerts:
+			real_period = {'1': 1, '2': 7, '3': 14, '4': 28}
+			t_date = alert.start
+			
+			while t_date <= cur_date:
+				if t_date == cur_date:
+					alerts.append(alert)
+					break
+				else:
+					t_date += timedelta(days=real_period[alert.period])
+		
+		if len(alerts) != 0:
 			msges = []
 			
 			for alert in alerts:
 				subject = u'LinsAlert - оповещение о смене линз.'
-				msg = u'Здравствуйте, ' + alert.user.first_name + u'. Сервис LinsAlert напоминает вам об необходимости смены линз.'
+				msg = u'Здравствуйте, ' + alert.user.first_name + u'.\n\n Сервис LinsAlert напоминает вам об необходимости смены линз.'
 				from_email = u'shpuntik74@gmail.com'
 				
 				msges.append((subject, msg, from_email, [alert.email]))
